4081c6c74dd795509070a0ac7b43067fc77241aa
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-01 08:28:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,4 +18,9 @@ class InteractiveRecord
   def table_name_for_insert
     self.class.table_name
   end
+  
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name)
 end
