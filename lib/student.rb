1b44767e1a9a48020d0958cb295d2e83b434407b
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-01 08:04:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true 
+    sql = "pragma"
+  end
 
 end
